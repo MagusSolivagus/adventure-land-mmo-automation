8921ad1f0358d13d6d59ee7f003b26dd7e1b2d5e
Magus Solivagus <kirx1994@gmail.com>
2026-09-13 13:39:35 +0200

    Update Priest_class.js
    added tank_search(); and former hardcoded searches for "Gulrot" are now dynamic to the groups warrior instead.
    
    split the battle loops into group_battle() and solo_battle();
    
    seek_monster() added for solo_battle()
    
    added d_blesss_atk_buff() to increase highest atk group members damage by 25%

--- Priest_class.js
+++ Priest_class.js
@@ -1,13 +1,39 @@
-// Gulrot is my warrior and Kaal my merchant, currently they are hardcoded rather than dynamic.
-
 let attack_mode = false;
 
 let follow_mode = true;
 
 // these are all functions for the main loop. The main loop is at the bottom.
 
+// Checks who is in party and their stats (names, hp, mana, etc...)
+function whoarewe() {
+	let who_is_in_party = [] // empty list as placeholder
+	if (character.party === null) return who_is_in_party;
+	let party_names=Object.keys(get_party()); // extracts party member names
+	for (let name of party_names) { 		// loops through party_names
+		let memberstats = get_player(name); // put stats of name in variable
+		if (!memberstats) continue; // if None, continue without crash
+		who_is_in_party.push(memberstats); // put party into the placeholder
+	}
+	return who_is_in_party; // returns each members stats to function
+}
+
+//search for warrior in group
+function tank_search() {
+	let group = whoarewe();
+	if(!group) return;
+	let leader = null;
+	for(let member of group) {
+		if(member.ctype === "warrior") {
+			leader = member;
+			break;
+		}
+	}
+	return leader;
+}
+
+
 // join party - Gulrot
-function summonparty() {
+function summon_party() {
 	if (character.party != null) {
 		return;
 	}
@@ -17,9 +43,12 @@ function summonparty() {
 
 
 //switching follow/battle mechanic
-function updatecfmode() {
-	let leader = get_player("Gulrot");
-	if (leader == null) { return; }
+async function update_cf_mode() {
+	let leader = tank_search();
+	if (leader === null) { 
+		follow_mode = false;
+		return; 
+	}
 	let leader_actual_target = get_target_of(leader);
 	if(leader) {
 		if(leader_actual_target && !leader_actual_target.rip) {
@@ -31,25 +60,48 @@ function updatecfmode() {
 		}
 	}
 	if (follow_mode == true && distance(character, leader) > 15) {
-		set_message("Following");
-		xmove(
+		set_message(`Follow: ${leader.name}`);
+		await xmove(
 			character.x+(leader.x-character.x) * 0.8,
 			character.y+(leader.y-character.y) * 0.8
 			);
 	}
 }
 
-// Checks who is in party and their stats (names, hp, mana, etc...)
-function whoarewe() {
-	let who_is_in_party = [] // empty list as placeholder
-	if (character.party == null) return who_is_in_party;
-	let party_names=Object.keys(get_party()); // extracts party member names
-	for (let name of party_names) { 		// loops through party_names
-		let memberstats = get_player(name); // put stats of name in variable
-		if (!memberstats) continue; // if None, continue without crash
-		who_is_in_party.push(memberstats); // put party into the placeholder
+// seek nearest new target / prioritize phoenix
+function seek_monster() {
+	let target=get_targeted_monster();
+	if(target && target.rip) {
+		target = null
+	} //if no target or target is not type "phoenix"
+	if(!target || target.mtype !== "phoenix") { 
+		let is_pho = get_nearest_monster({ // search for phoenix nearby
+		type: "phoenix",
+		})
+		if(is_pho) {
+			set_message(`Found: ${is_pho.name}`); 
+			change_target(is_pho); //if found, target it immediently
+			game_log(`${character.name} found a ${is_pho.name}`); 
+			return is_pho; // return phoenix to overwrite old target 
+		}
+	} 
+	if(target && !target.rip) {
+	   return target;
 	}
-	return who_is_in_party; // returns each members stats to function
+	if(!target) {
+		target = get_nearest_monster({
+		min_xp:100,
+		max_att:200,
+		max_distance:20,
+		path_check:true
+		})
+		if(target) {
+			set_message(`Found: ${target.name}`)
+			change_target(target);
+			return target;
+		}
+	}
+	return target;
 }
 
 // sending items and gold to Kaal if online and neaby
@@ -72,13 +124,13 @@ function send_to_Kaal() {
 function drink_potion() {
 	// if 33% hp, use a health potion
 	if (character.hp < character.max_hp / 3 && can_use("use_hp")) {
-		set_message("Drink Potion");
+		set_message("Drink HP");
 		use_skill('use_hp');
 		return;
 	}
 	// if 50% mana, use mana potion
 	if (character.mp < character.max_mp / 2 && can_use("use_mp")) {
-		set_message("Drink Potion");
+		set_message("Drink MP");
 		use_skill('use_mp');
 		return;
 	}
@@ -97,10 +149,10 @@ function drink_potion() {
 }
 
 // Use standard heal skill
-function smallheal() {
+function small_heal() {
 	if (character.party == null) {
 		if (character.hp < character.max_hp * 0.75 && can_use("heal")) {
-			set_message(`Healing ${character}`);
+			set_message(`Healing ${character.name}`);
 			use_skill("heal", character);	
 		}
 		return;
@@ -127,14 +179,15 @@ function smallheal() {
 	}
 }
 
-function pheal() {
+function party_heal() {
 	let members = whoarewe();
 	for(let member of members) {
 		if(member.hp < member.max_hp / 4 && character.mp > 400 && can_use("partyheal")) {
-				use_skill("partyheal");
-				set_message("Party Heal!");
+			use_skill("partyheal");
+			set_message("Party Heal!");
+			return;
 		} else if (character.mp < 400 && can_use("use_mp")) {
-			set_message("OOM!");
+			set_message("OOM P-Heal");
 			use_skill("use_mp");
 			return; 
 		}
@@ -142,35 +195,42 @@ function pheal() {
 }
 
 // curse skill for harder enemies
-function curseboss() {
-	let leader = get_player("Gulrot")
-	let target = get_target_of(leader)
+function curse_boss() {
+	let leader = tank_search();
+	if(!leader) {
+		let s_target = character.target;
+		if(s_target.max_hp > character.max_hp * 10 && character.mp > 400) {
+			if(can_use("curse")) {
+				use_skill("curse", s_target);
+				set_message(`Curse ${s_target.name}`);
+				return;
+			}
+		}
+	}
+	let target = get_target_of(leader);
 	if(!target) return;
-	if(target.slots && target.slots.cursed) {
+	if(target.s === "cursed") {
 		return;
 	}
 	if(target.max_hp > leader.max_hp * 10 && character.mp > 400) {
 		if(can_use("curse")) {
 			use_skill("curse", target);
-			set_message("Cursing Enemy");
+			set_message(`Curse ${target.name}`);
+			return;
 		}
 	} 
 	else if (character.mp < 400 && can_use("use_mp")) {
-		set_message("OOM!");
+		set_message("OOM Curse");
 		use_skill("use_mp");
 		return; 
 	}
 }
 
-
-// In group standard attack
-	
-function ltattack() {
-	let leader = get_player("Gulrot");
-   	let target = get_target_of(leader);
+// solo_attack
+function solo_attack() {
+	let target = seek_monster();
 	if (!target || target.rip) return;
-	if(!is_in_range(target))
-	{
+	if(!is_in_range(target)) {
 		move(
 			character.x+(target.x-character.x)/2,
 			character.y+(target.y-character.y)/2
@@ -178,15 +238,38 @@ function ltattack() {
 		// Walk half the distance
 	}
 	else if(can_attack(target)) {
-		set_message("Attacking");
+		set_message(`Atk on: ${target.name}`);
         attack(target);
 		return;
 	}
 }
 
-// using "Absorb Sins" to pull aggro from low HP party
-function tank4lowparty() {
+
+// In group standard attack
+function lt_attack() {
+	let leader = tank_search();
+	if(!leader) return;
+   	let target = get_target_of(leader);
+	if (!target || target.rip) return;
+	if(!is_in_range(target)) {
+		move(
+			character.x+(target.x-character.x)/2,
+			character.y+(target.y-character.y)/2
+			);
+		// Walk half the distance
+	}
+	else if(can_attack(target)) {
+		set_message(`Atk on: ${target.name}`);
+        attack(target);
+		return;
+	}
+}
+
+// using "Absorb Sins" to pull aggro away from low HP party
+function tank_low_party() {
+	if(character.level < 55) return;
 	let group = whoarewe();
+	if(!group) return;
 	for( let member of group) {
 		if(member.hp < member.max_hp / 4 && can_use("absorb")) {
 			use_skill("absorb")
@@ -196,14 +279,62 @@ function tank4lowparty() {
 	}
 }
 
+// Dark blessing, buff atk by 25%
+function d_bless_atk_buff() {
+	if(character.level < 70) return;
+	let group = whoarewe();
+	if(!group) {
+		if(character.mp >= 900 && can_use("darkblessing")) {
+			set_message(`Blessing ${character.name}!`);
+			use_skill("darkblessing");
+			return;
+		}
+	}
+	group.sort = (function(a, b) { // sort member atk stats
+		return (b.atk) - (a.atk);	   // highest atk is first in list
+	});
+	let top_atk_member = group[0];  // grab highest atk member
+	if(character.mp >= 900 && can_use("darkblessing")) {
+		set_message(`Blessing ${top_atk_member.name}!`);
+		use_skill("darkblessing", top_atk_member);
+		return;
+	}
+}
+
+// solo battle
+function solo_battle() {
+	
+	small_heal();
+	
+	d_bless_atk_buff();
+	
+	solo_attack();
+}
+// group battle 
+function group_battle() {
+	// pull aggro from low party members
+	tank_low_party()
+	
+	//partyheal if below 25% hp
+	party_heal();
+	// single target standard heal if member at 75% hp
+	small_heal();
+	
+	// curse tougher enemies
+	curse_boss();
+	
+	// Use standard attack on leader target
+	lt_attack();
+}
+
 
 // accept Gulrots party invite every 10 seconds
 setInterval(function(){ 
-	summonparty();
+	summon_party();
 }, 1000 * 10);
 
 setInterval(function(){ 
-	updatecfmode();
+	update_cf_mode();
 }, 1000);
 
 //mainloop every 0.25 seconds
@@ -215,19 +346,14 @@ setInterval(function(){
 	
 	loot();
 	
-	if(!attack_mode || character.rip || character.party == null) return;
-
-	// Heal and battle loops
-
-	//partyheal if below 25% hp
-	pheal();
-	// single target standard heal if member at 75% hp
-	smallheal();
+	if(!attack_mode || character.rip) return;
 	
-	// curse tougher enemies
-	curseboss();
+	if(character.party !== null) {
+		group_battle();
+	}
+	else {
+		solo_battle();
+	}
 	
-	// Use standard attack on leader target
-	ltattack();
 
 },1000/4); // Loops every 1/4 seconds.
