3e1e0d7dfa1a512b3a8d06485da8e7c6f6295200
Magus Solivagus <kirx1994@gmail.com>
2026-09-15 18:45:13 +0200

    Update Merchant.js
    Merchant now automatically searches for my warrior "Gulrot" using "receive_pos_goto_gul()" function. (might be a bit bloated right now since its doing multiple things at once)
    
    merchant now automatically delivers fresh mana potions to figher group using buy_mana_potions() and mana_delivery() which is inweaved into receive_pos_goto_gul().
    
    While on delivery run, merchant receives loot and gold from fighter group and sells specific items after being back in town using sellable_items()
    
    new playersearch() now scans nearby player characters objects.
    
    using playersearch() to buff nearby players that are NOT merchants with mluck skill.

--- Merchant.js
+++ Merchant.js
@@ -1,20 +1,109 @@
 // Merchant class - buys, stores and sells stuff, cannot fight
 
-// selling specific items, doesnt work yet needs more thought
-function selltrash() {
-	for (let i = 0; i < 42; i++) {
-		let item = character.items[i];
-		if (!item) continue; 
-		if (item.name == "ringsj" || item.name == "hpamulet" || item.name == "hpbelt") {
-			let sellernpc = find_npc("Gabriel");
-			if(!is_in_range(sellernpc)) {
-				return;
-			}
-			sell(i, 1)
-		}
-    }
+// Checks who is in party and their stats (names, hp, mana, etc...)
+function whoarewe() {
+	let who_is_in_party = [] // empty list as placeholder
+	if (character.party == null) return who_is_in_party;
+	let party_names=Object.keys(get_party()); // extracts party member names
+	for (let name of party_names) { 		// loops through party_names
+		let memberstats = get_player(name); // names get into a variable
+		if (!memberstats) continue; // if None, continue without crash
+		who_is_in_party.push(memberstats); // put party into the placeholder
+	}
+	return who_is_in_party; // returns each members stats to function
 }
 
+
+function buy_mana_potions() {
+	let mp_needed = Math.max(0, 1500 - quantity("mpot0"));
+	if(mp_needed) {
+		buy_with_gold("mpot0", mp_needed);
+		game_log(`Restocked ${mp_needed} Mana Potions`);
+	}
+}
+
+// give potions to fighers
+function mana_delivery() {
+	let party_members = whoarewe();
+	if(!party_members) return;
+	let mana_pot = locate_item("mpot0");
+	for(let member of party_members) {
+		if(member !== character) {
+			send_item(member,mana_pot, 500);
+			game_log(`send ${member.name} Mana-Potions!`);
+		}
+	}
+}
+// at the moment, sell specific items found by fighter group
+function sellable_items() {
+	let sellme = [];
+	for(let i = 0; i < 42; i++) {
+		let item = character.items[i];
+        if (!item) continue; 
+        if (item.name == "vitring" ||
+			item.name == "strring" ||
+			item.name == "intring" ||
+			item.name == "dexring" ||
+			item.name == "wattire") {
+				sellme.push(i);
+		}
+	}
+	for(let slot of sellme) {
+		let items_to_sell = character.items[slot];
+		if(items_to_sell) {
+			sell(slot, 1);
+		}
+	}
+}
+
+// get current position in world
+function current_position() {
+	let cur_pos = [];
+	cur_pos.push(character.map);
+	cur_pos.push(character.x);
+	cur_pos.push(character.y);
+	return cur_pos;
+}
+
+// receive position of Gulrot, go to him and collect loot/money, return to main
+// runs everytime Kaal receives data from Gulrot, every 15 minutes
+// received data comes in an array as above in the current_position() function
+async function receive_pos_goto_gul(name,data) {
+	if(name !== "Gulrot") return;
+	close_stand();
+	let gul_map = data[0];
+	let gul_x = data[1];
+	let gul_y = data[2];
+	if(character.map !== gul_map && !is_moving(character)) { 
+		await smart_move(gul_map);
+	}
+	await smart_move({
+		x: gul_x,
+		y: gul_y
+	});
+	mana_delivery();
+	await sleep(1000 * 60);
+	use_skill("use_town");
+	await sleep(1000 * 5);
+	await smart_move("main");
+	await sleep(1000 * 2)
+	await smart_move("fancypots");
+	await buy_mana_potions();
+	await sellable_items();
+	await smart_move({
+		x: -116,
+		y: 14
+		});
+	await sleep(1000 * 2)
+	open_stand();
+}
+
+function exchange_items_xyn(itemName) {
+    let itemIndex = character.items.findIndex(item => item && item.name === itemName);
+    if (itemIndex !== -1) {
+        exchange(itemIndex);
+    }
+}
 
 // passive  regeneration
 function regenerate() {
@@ -45,6 +134,19 @@ function playersearch() {
 	return playerlist; // return list when function is called
 }
 
+function mluck_buff() {
+	let players_around_me = playersearch();
+	for(let player of players_around_me) {
+		let no_buff = !player.s || !player.s["mluck"];
+		let buff_from_others = player.s && player.s["mluck"] && player.s["mluck"].f !== character.name;
+		if((no_buff || buff_from_others) && player.ctype !== "merchant" && is_in_range(player)) {
+			set_message("M-Luck buff!");
+			use_skill("mluck", player);
+			return;
+		}
+	}
+}
+
 //Mass Produce skill
 function mass_produce() {
 	if(character.level < 30) {
@@ -55,25 +157,25 @@ function mass_produce() {
 		use_skill("massproduction", character);
 		return;
 	}
-	/* // mechanic to read other players buff, if they dont have it, buff them
-	// the massproduction skill seems to be self cast only :(
-	let players_around = playersearch(); // call function and save value in variable
-	if(can_use("massproduction") && character.mp >= character.max_mp - 100) {
-		for(let target_player of players_around) { // loop through players
-			if(target_player.s.massproduction) { // if player has buff, skip them
-				continue;
-			}
-			set_message("Mass Prod.");
-			use_skill("massproduction", target_player);
-		}
-	}
-	*/
+
+}
+// sleeper agent, waits for right command and instantly activates
+function on_cm(name,data) {
+	receive_pos_goto_gul(name,data);
 }
 
 setInterval(function(){
+	
+	mluck_buff();
 	
 	mass_produce();
 	
 	regenerate();
 	
 },1000/4); // Loops every 1/4 seconds.
+
+setInterval(function(){
+	
+	//exchange_items_xyn("marketparcel");
+	
+},1000 * 2); // loops every 2 seconds
